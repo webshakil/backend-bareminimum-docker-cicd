82c6141ec45f1c9f58100c81e24862fc449d935f
webshakil <chumkysk@gmail.com>
2024-05-25 12:14:14 +0600

    dbconnect modified

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,19 @@
-
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config() 
+
+dotenv.config();
+
 const createMongoURI = () => {
   const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DB_NAME } = process.env;
- 
-  if (!MONGO_USERNAME || !MONGO_PASSWORD || !MONGO_DB_NAME ) {
+
+  if (!MONGO_USERNAME || !MONGO_PASSWORD || !MONGO_DB_NAME) {
     throw new Error('Missing required environment variables for MongoDB connection.');
   }
-
   const uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@mernstack-pagination.dmkoc.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
-  //MONGO_URI="mongodb+srv://topu:Hphp2010f@mernstack-pagination.dmkoc.mongodb.net/rayan-typescript-mern-3?retryWrites=true&w=majority"
+
+  mongoose.connect(uri)
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
   return uri;
 };
